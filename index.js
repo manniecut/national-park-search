dbcf6dc03bce5ce0aa9de43fdd37311d51cded45
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-08-29 17:32:33 -0400

    setting up

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+/*
+Display a list of national parks in an area.
+-Review The National Parks Services API documentation and create an API key.
+    https://www.nps.gov/subjects/developer/get-started.htm
+-Review the API Guide on Authentication for ways to pass your API key as part of the request.
+    https://www.nps.gov/subjects/developer/guides.htm
+-Review the /parks endpoint and data model to understand how it works.
+    https://www.nps.gov/subjects/developer/api-documentation.htm#/parks/getPark
+    https://www.nps.gov/subjects/developer/api-documentation.htm#/definitions/Park
+
+When you're done, submit the link to your GitHub repo at the bottom of the page.
+Requirements:
+-The user must be able to search for parks in one or more states.
+-The user must be able to set the max number of results, with a default of 10.
+-The search must trigger a call to NPS's API.
+-The parks in the given state must be displayed on the page. Include at least:
+Full name
+Description
+Website URL
+-The user must be able to make multiple searches and see only the results for the current search.
+-As a stretch goal, try adding the park's address to the results.
+*/
+
+
+
+function getParks() {
+
+}
+
+
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchInput = $('#js-search-input').val();
+        getParks(searchInput);
+
+    })
+}
+
+
+$(watchForm);
